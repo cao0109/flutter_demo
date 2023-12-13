97b8c30c94e72d7f04a0900714ce2542396f6cdb
cao0109 <102588999+cao0109@users.noreply.github.com>
2023-12-14 04:58:10 +0800

    Update  bottomNavigationBar

--- lib/pages/system/main/main_view.dart
+++ lib/pages/system/main/main_view.dart
@@ -5,17 +5,61 @@ import 'package:get/get.dart';
 import 'main_controller.dart';
 
 class MainView extends StatefulWidget {
-  const MainView({super.key});
+  const MainView({Key? key}) : super(key: key);
 
   @override
   State<MainView> createState() => _MainViewState();
 }
 
-class _MainViewState extends State<MainView> {
+class _MainViewState extends State<MainView>
+    with AutomaticKeepAliveClientMixin {
+  @override
+  bool get wantKeepAlive => true;
+
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+    return const _MainViewGetX();
+  }
+}
+
+class _MainViewGetX extends GetView<MainController> {
+  const _MainViewGetX({Key? key}) : super(key: key);
+
+  Widget _tab(
+      {required int index,
+      required IconData icon,
+      required IconData selectedIcon,
+      required String text}) {
+    return GestureDetector(
+      onTap: () => controller.changePage(index),
+      child: Obx(
+        () => Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(
+              controller.currentIndex.value == index ? selectedIcon : icon,
+              color: controller.currentIndex.value == index
+                  ? Get.theme.colorScheme.primary
+                  : null,
+            ),
+            Text(
+              text,
+              style: TextStyle(
+                color: controller.currentIndex.value == index
+                    ? Get.theme.colorScheme.primary
+                    : null,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return GetBuilder<MainController>(
-      init: MainController(),
       builder: (_) {
         return Scaffold(
           body: Obx(
@@ -31,36 +75,36 @@ class _MainViewState extends State<MainView> {
             ),
             child: BottomAppBar(
               shape: const CircularNotchedRectangle(),
-              child: Container(
-                decoration: const BoxDecoration(),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Obx(
-                      () => IconButton(
-                        onPressed: () {
-                          _.changePage(0);
-                        },
-                        icon: const Icon(Icons.home_outlined),
-                        selectedIcon: const Icon(Icons.home_filled),
-                        isSelected: _.currentIndex.value == 0,
-                      ),
-                    ),
-                    Obx(
-                      () => IconButton(
-                        onPressed: () {
-                          _.changePage(1);
-                        },
-                        icon: const Icon(Icons.person_outline),
-                        selectedIcon: const Icon(Icons.person),
-                        isSelected: _.currentIndex.value == 1,
-                      ),
-                    ),
-                  ],
-                ),
+              notchMargin: 8,
+              height: 70,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  _tab(
+                    index: 0,
+                    icon: Icons.home_outlined,
+                    selectedIcon: Icons.home,
+                    text: "home".tr,
+                  ),
+                  _tab(
+                    index: 1,
+                    icon: Icons.settings_outlined,
+                    selectedIcon: Icons.settings,
+                    text: "setting".tr,
+                  ),
+                ],
               ),
             ),
           ),
+          floatingActionButtonLocation:
+              FloatingActionButtonLocation.centerDocked,
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            tooltip: "add".tr,
+            shape: const CircleBorder(),
+            child: const Icon(Icons.add),
+          ),
+          floatingActionButtonAnimator: FloatingActionButtonAnimator.scaling,
         );
       },
     );
